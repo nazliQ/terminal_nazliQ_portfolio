f808765fbc0f45f0accb6b307beaf42f81fc992b
Nathalie Piqué Kulsrud <101467994+nazliQ@users.noreply.github.com>
2024-03-27 15:51:45 +0100

    intital commit

---
+++ buttons.js
@@ -0,0 +1,5 @@
+document.getElementById('resize-btn').addEventListener('click', function() {
+  const terminal = document.getElementById('terminal');
+  // Toggle between bigger and normal size
+  terminal.classList.toggle('large');
+});
